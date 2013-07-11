affd3a0d047b22afdfe1af084cbc63eafb774d44
mcguffin <joern@flyingletters.com>
2013-07-11 10:41:03 +0200

    updated plugin description

---
+++ wp-revision-request.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* @package RevisionRequest
+* @version 0.1
+*/
+
+/*
+Plugin Name: RevisionRequest
+Plugin URI: https://github.com/mcguffin/wp-revision-request
+Description: More revision control: Create revision only upon request, delete revisions.
+Author: Joern Lund
+Version: 0.1
+Author URI: http://flyingletters.com
+*/
+
+
+
+?>
