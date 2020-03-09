f906d153b5c81195f0be49827260d2f4c2cf51fd
mcguffin <joern@flyingletters.com>
2020-03-09 11:16:50 +0100

    EOL

--- wp-revision-request.php
+++ wp-revision-request.php
@@ -7,7 +7,7 @@
 /*
 Plugin Name: RevisionRequest
 Plugin URI: http://wordpress.org/plugins/wp-revision-request/
-Description: <strong>NO LONGER MAINTAINED.</strong> More revision control: Create revision only upon request. Controls to delete revisions. Display revisions on the blog.
+Description: <strong>PLUGIN ABANDONED!</strong> More revision control: Create revision only upon request. Controls to delete revisions. Display revisions on the blog.
 Author: Joern Lund
 Version: 1.0.2
 Author URI: https://github.com/mcguffin
@@ -26,4 +26,28 @@ if ( is_admin() ) {
 }
 require_once( dirname(__FILE__). '/inc/class-revisiondisplay.php' );
 
-?>
+
+
+function wp_revisionrequest_deprecation_notice() {
+/*
+'<tr class="plugin-update-tr%s" id="%s" data-slug="%s" data-plugin="%s">' .
+
+*/
+	?>
+	<tr class="plugin-update-tr active" >
+		<td class="plugin-update colspanchange" colspan="3" style="position:relative;top:-1px;">
+			<div class="notice notice-error notice-alt inline">
+				<p>
+					<span class="dashicons dashicons-warning"></span>
+					<strong><?php esc_html_e('Warning:','revisionrequest'); ?></strong>
+					<?php esc_html_e('“RevisionRequest” is no longer maintained. It may disappear without further notice.','revisionrequest'); ?>
+				</p>
+			</div>
+		</td>
+	</tr>
+	<?php
+}
+
+if ( is_admin() ) {
+	add_action('after_plugin_row_wp-revision-request/wp-revision-request.php','wp_revisionrequest_deprecation_notice');
+}
